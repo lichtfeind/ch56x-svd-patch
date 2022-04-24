3adfade7bc515d8f53be91a2f44472dd619f4492
Jonas Martin <lichtfeind@gmail.com>
2022-04-24 12:45:20 +0200

    Move output into sub folder

--- build.sh
+++ build.sh
@@ -4,7 +4,11 @@
 ~/.local/bin/svd patch devices/ch569.yaml
 
 svd2rust -g --target=riscv -i CH56Xxx.svd.patched
-rm -rf src
-form -i lib.rs -o src/ && rm lib.rs
-mv generic.rs src/
+rm -rf ch569-rs
+
+mkdir ch569-rs
+form -i lib.rs -o ch569-rs/src/ && rm lib.rs
+mv generic.rs ch569-rs/src/
+mv build.rs ch569-rs/
+cp Cargo.toml ch569-rs/
 cargo fmt
