f38e3a9087c1b5b5ae7914e933910add30f691fd
Jonas Martin <lichtfeind@gmail.com>
2022-04-24 12:56:43 +0200

    Rename Cargo.toml and add device.x

--- build.sh
+++ build.sh
@@ -10,5 +10,7 @@ mkdir ch569-rs
 form -i lib.rs -o ch569-rs/src/ && rm lib.rs
 mv generic.rs ch569-rs/src/
 mv build.rs ch569-rs/
-cp Cargo.toml ch569-rs/
-cargo fmt
+mv device.x ch569-rs/
+cp ch569-rs.toml ch569-rs/Cargo.toml
+
+cd ch569-rs && cargo fmt
